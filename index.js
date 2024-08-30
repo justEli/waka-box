c66745fb04b2a90fc5452b65693bc619c4eaaae9
Eli <JustEli@users.noreply.github.com>
2024-08-30 10:52:17 +0200

    fix ugly percentage bar

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function updateGist(stats) {
     const line = [
       trimRightStr(name, 10).padEnd(10),
       time.padEnd(14),
-      generateBarChart(percent, 21),
+      generateBarChart(percent, 25),
       String(percent.toFixed(1)).padStart(5) + "%"
     ];
 
@@ -65,18 +65,13 @@ async function updateGist(stats) {
 }
 
 function generateBarChart(percent, size) {
-  const syms = "░▏▎▍▌▋▊▉█";
-
-  const frac = Math.floor((size * 8 * percent) / 100);
-  const barsFull = Math.floor(frac / 8);
-  if (barsFull >= size) {
-    return syms.substring(8, 9).repeat(size);
+  const filledCells = Math.round((size * percent) / 100);
+  
+  if (filledCells >= size) {
+    return '▓'.repeat(size);
   }
-  const semi = frac % 8;
 
-  return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
-    .join("")
-    .padEnd(size, syms.substring(0, 1));
+  return '▓'.repeat(filledCells).padEnd(size, '░');
 }
 
 (async () => {
